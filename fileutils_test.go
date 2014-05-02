a53a5615ae423a30334aaf1a72ff0748dcb67764
Rafe Colton <r.colton@modcloth.com>
2014-05-01 18:37:07 -0700

    Filling out project basics

---
+++ fileutils_test.go
@@ -0,0 +1,22 @@
+package fileutils_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestBuilder(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Fileutils Spec")
+}
+
+var _ = Describe("", func() {
+	var ()
+
+	BeforeEach(func() {
+	})
+
+	XIt("", func() {
+	})
+})
